ff04ba1047aa7befcc9f0cb25b0624a48dc70159
ezassaye <87672860+ezassaye@users.noreply.github.com>
2022-07-27 13:26:48 -0700

    lesson

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+let buttonClicked= function(){
+    console.log("it works!")
+}
+let addListener = function(btn){
+    btn.addEventListener("clicked",buttonClicked);
+}
+let count = 0;
+let value = document.querySelector("#value");
+let btns = document.querySelectorAll("button");
+btns.forEach(addListeners);
 
